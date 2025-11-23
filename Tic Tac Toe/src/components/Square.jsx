c73f7ab59fd8892f220f1800dce83d7b0d1cf3ca
Iland <iland12maimom@gmail.com>
2025-11-23 07:53:19 +0530

    first

---
+++ Tic Tac Toe/src/components/Square.jsx
@@ -0,0 +1,13 @@
+import "./Square.css";
+
+function Square(props) {
+  const className = props.isWinning ? "square square--winning" : "square";
+
+  return (
+    <button className={className} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
